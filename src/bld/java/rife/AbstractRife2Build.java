0d07c4856946555154afdd26e54d6a855fe3724f
Geert Bevin <gbevin@uwyn.com>
2023-05-11 12:28:56 -0400

    Split build file into a reusable abstract class and the core build file

---
+++ src/bld/java/rife/AbstractRife2Build.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2001-2023 Geert Bevin (gbevin[remove] at uwyn dot com)
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
+package rife;
+
+import rife.bld.BuildCommand;
+import rife.bld.Project;
+import rife.bld.extension.Antlr4Operation;
+import rife.bld.extension.TestsBadgeOperation;
+import rife.bld.operations.JavaOptions;
+import rife.tools.FileUtils;
+import rife.tools.StringUtils;
+
+import java.io.File;
+import java.util.List;
+
+import static rife.bld.dependencies.Repository.MAVEN_CENTRAL;
+import static rife.bld.dependencies.Repository.RIFE2_RELEASES;
+import static rife.bld.dependencies.Scope.test;
+import static rife.bld.operations.JavadocOptions.DocLinkOption.NO_MISSING;
+
+public class AbstractRife2Build extends Project {
+    public AbstractRife2Build()
+    throws Exception {
+        pkg = "rife";
+
+        javaRelease = 17;
+        downloadSources = true;
+        autoDownloadPurge = true;
+
+        repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
+        scope(test)
+            .include(dependency("org.slf4j", "slf4j-simple", version(2, 0, 7)))
+            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 9, 3)))
+            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 9, 3)))
+            .include(dependency("com.h2database", "h2", version(2, 1, 214)))
+            .include(dependency("net.sourceforge.htmlunit", "htmlunit", version(2, 70, 0)))
+            .include(dependency("org.postgresql", "postgresql", version(42, 6, 0)))
+            .include(dependency("com.mysql", "mysql-connector-j", version(8, 0, 33)))
+            .include(dependency("org.mariadb.jdbc", "mariadb-java-client", version(3, 1, 4)))
+            .include(dependency("org.hsqldb", "hsqldb", version(2, 7, 1)))
+            .include(dependency("org.apache.derby", "derby", version("10.16.1.1")))
+            .include(dependency("org.apache.derby", "derbytools", version("10.16.1.1")))
+            .include(dependency("com.oracle.database.jdbc", "ojdbc11", version("23.2.0.0")));
+
+        cleanOperation()
+            .directories(
+                new File(workDirectory(), "embedded_dbs"),
+                new File(workDirectory(), "logs"));
+
+        antlr4Operation
+            .visitor()
+            .longMessages();
+
+        propagateJavaProperties(testsBadgeOperation.javaOptions(),
+            "test.postgres",
+            "test.mysql",
+            "test.mariadb",
+            "test.oracle",
+            "test.oracle-free",
+            "test.derby",
+            "test.hsqldb",
+            "test.h2");
+
+        javadocOperation()
+            .excluded(
+                "rife/antlr/",
+                "rife/asm/",
+                "rife/.*/databasedrivers/",
+                "rife/.*/imagestoredrivers/",
+                "rife/.*/rawstoredrivers/",
+                "rife/.*/textstoredrivers/",
+                "rife/database/capabilities/"
+            )
+            .javadocOptions()
+                .docLint(NO_MISSING)
+                .keywords()
+                .splitIndex()
+                .tag("apiNote", "a", "API Note:")
+                .link("https://jakarta.ee/specifications/servlet/5.0/apidocs/")
+                .link("https://jsoup.org/apidocs/");
+    }
+
+    void propagateJavaProperties(JavaOptions options, String... names) {
+        for (var name : names) {
+            if (properties().contains(name)) {
+                options.property(name, properties().getValueString(name));
+            }
+        }
+    }
+
+    final Antlr4Operation antlr4Operation = new Antlr4Operation() {
+        @Override
+        public void execute()
+        throws Exception {
+            super.execute();
+            // replace the package name so that it becomes part of RIFE2
+            FileUtils.transformFiles(outputDirectory(), FileUtils.JAVA_FILE_PATTERN, null, s ->
+                StringUtils.replace(s, "org.antlr.v4.runtime", "rife.antlr.v4.runtime"));
+        }
+    };
+
+    @BuildCommand(summary = "Generates the grammar Java sources")
+    public void generateGrammar()
+    throws Exception {
+        antlr4Operation.executeOnce();
+    }
+
+    public void compile()
+    throws Exception {
+        generateGrammar();
+        super.compile();
+    }
+
+    final TestsBadgeOperation testsBadgeOperation = new TestsBadgeOperation();
+    public void test()
+    throws Exception {
+        testsBadgeOperation.executeOnce(() -> testsBadgeOperation
+            .url(property("testsBadgeUrl"))
+            .apiKey(property("testsBadgeApiKey"))
+            .fromProject(this));
+    }
+}
--- src/bld/java/rife/CoreBuild.java
+++ src/bld/java/rife/CoreBuild.java
@@ -4,60 +4,25 @@
  */
 package rife;
 
-import rife.bld.BuildCommand;
-import rife.bld.Project;
-import rife.bld.extension.Antlr4Operation;
-import rife.bld.extension.TestsBadgeOperation;
 import rife.bld.operations.*;
 import rife.bld.publish.*;
 import rife.tools.FileUtils;
-import rife.tools.StringUtils;
 
 import java.io.File;
 import java.util.List;
 
-import static rife.bld.dependencies.Repository.*;
-import static rife.bld.dependencies.Scope.*;
-import static rife.bld.operations.JavadocOptions.DocLinkOption.NO_MISSING;
 import static rife.bld.operations.TemplateType.*;
 
-public class CoreBuild extends Project {
+public class CoreBuild extends AbstractRife2Build {
     public CoreBuild()
     throws Exception {
-        pkg = "rife";
         name = "rife2-core";
 
         version = version(FileUtils.readString(new File(srcMainResourcesDirectory(), "CORE_VERSION")));
 
-        javaRelease = 17;
-        downloadSources = true;
-        autoDownloadPurge = true;
-
-        repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
-        scope(test)
-            .include(dependency("org.slf4j", "slf4j-simple", version(2, 0, 7)))
-            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 9, 3)))
-            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 9, 3)))
-            .include(dependency("com.h2database", "h2", version(2, 1, 214)))
-            .include(dependency("net.sourceforge.htmlunit", "htmlunit", version(2, 70, 0)))
-            .include(dependency("org.postgresql", "postgresql", version(42, 6, 0)))
-            .include(dependency("com.mysql", "mysql-connector-j", version(8, 0, 33)))
-            .include(dependency("org.mariadb.jdbc", "mariadb-java-client", version(3, 1, 4)))
-            .include(dependency("org.hsqldb", "hsqldb", version(2, 7, 1)))
-            .include(dependency("org.apache.derby", "derby", version("10.16.1.1")))
-            .include(dependency("org.apache.derby", "derbytools", version("10.16.1.1")))
-            .include(dependency("com.oracle.database.jdbc", "ojdbc11", version("23.2.0.0")));
-
-        cleanOperation()
-            .directories(
-                new File(workDirectory(), "embedded_dbs"),
-                new File(workDirectory(), "logs"));
-
         antlr4Operation
             .sourceDirectories(List.of(new File(srcMainDirectory(), "antlr")))
-            .outputDirectory(new File(buildDirectory(), "generated/rife/template/antlr"))
-            .visitor()
-            .longMessages();
+            .outputDirectory(new File(buildDirectory(), "generated/rife/template/antlr"));
 
         precompileOperation()
             .templateTypes(HTML, XML, SQL);
@@ -65,37 +30,11 @@ public class CoreBuild extends Project {
         compileOperation()
             .mainSourceDirectories(antlr4Operation.outputDirectory())
             .compileOptions()
-            .debuggingInfo(JavacOptions.DebuggingInfo.ALL);
-
-        propagateJavaProperties(testsBadgeOperation.javaOptions(),
-            "test.postgres",
-            "test.mysql",
-            "test.mariadb",
-            "test.oracle",
-            "test.oracle-free",
-            "test.derby",
-            "test.hsqldb",
-            "test.h2");
+                .debuggingInfo(JavacOptions.DebuggingInfo.ALL);
 
         javadocOperation()
-            .excluded(
-                "rife/antlr/",
-                "rife/asm/",
-                "rife/.*/databasedrivers/",
-                "rife/.*/imagestoredrivers/",
-                "rife/.*/rawstoredrivers/",
-                "rife/.*/textstoredrivers/",
-                "rife/database/capabilities/"
-            )
             .javadocOptions()
-            .docTitle("<a href=\"https://rife2.com\">RIFE2/core</a> " + version())
-            .docLint(NO_MISSING)
-            .keywords()
-            .splitIndex()
-            .tag("apiNote", "a", "API Note:")
-            .link("https://jakarta.ee/specifications/servlet/5.0/apidocs/")
-            .link("https://jsoup.org/apidocs/")
-            .overview(new File(srcMainJavaDirectory(), "overview.html"));
+                .docTitle("<a href=\"https://rife2.com\">RIFE2/core</a> " + version());
 
         publishOperation()
             .repository(version.isSnapshot() ? repository("rife2-snapshots") : repository("rife2-releases"))
@@ -120,46 +59,6 @@ public class CoreBuild extends Project {
                     .url("https://github.com/rife2/core"))
                 .signKey(property("sign.key"))
                 .signPassphrase(property("sign.passphrase")));
-    }
-
-    void propagateJavaProperties(JavaOptions options, String... names) {
-        for (var name : names) {
-            if (properties().contains(name)) {
-                options.property(name, properties().getValueString(name));
-            }
-        }
-    }
-
-    final Antlr4Operation antlr4Operation = new Antlr4Operation() {
-        @Override
-        public void execute()
-        throws Exception {
-            super.execute();
-            // replace the package name so that it becomes part of RIFE2
-            FileUtils.transformFiles(outputDirectory(), FileUtils.JAVA_FILE_PATTERN, null, s ->
-                StringUtils.replace(s, "org.antlr.v4.runtime", "rife.antlr.v4.runtime"));
-        }
-    };
-
-    @BuildCommand(summary = "Generates the grammar Java sources")
-    public void generateGrammar()
-    throws Exception {
-        antlr4Operation.executeOnce();
-    }
-
-    public void compile()
-    throws Exception {
-        generateGrammar();
-        super.compile();
-    }
-
-    private final TestsBadgeOperation testsBadgeOperation = new TestsBadgeOperation();
-    public void test()
-    throws Exception {
-        testsBadgeOperation.executeOnce(() -> testsBadgeOperation
-            .url(property("testsBadgeUrl"))
-            .apiKey(property("testsBadgeApiKey"))
-            .fromProject(this));
     }
 
     public static void main(String[] args)

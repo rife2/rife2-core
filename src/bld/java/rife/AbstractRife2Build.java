29a05f06cca053665086f206dbbc5834e27a4d7c
Geert Bevin <gbevin@uwyn.com>
2023-11-24 10:36:54 -0500

    Updated dependencies to latest versions

--- src/bld/java/rife/AbstractRife2Build.java
+++ src/bld/java/rife/AbstractRife2Build.java
@@ -32,16 +32,16 @@ public class AbstractRife2Build extends Project {
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
         scope(test)
             .include(dependency("org.slf4j", "slf4j-simple", version(2,0,9)))
-            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,10,0)))
-            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,10,0)))
+            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,10,1)))
+            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,10,1)))
             .include(dependency("com.h2database", "h2", version(2,2,224)))
             .include(dependency("net.sourceforge.htmlunit", "htmlunit", version(2,70,0)))
-            .include(dependency("org.postgresql", "postgresql", version(42,6,0)))
-            .include(dependency("com.mysql", "mysql-connector-j", version(8,1,0)))
-            .include(dependency("org.mariadb.jdbc", "mariadb-java-client", version(3,2,0)))
+            .include(dependency("org.postgresql", "postgresql", version(42,7,0)))
+            .include(dependency("com.mysql", "mysql-connector-j", version(8,2,0)))
+            .include(dependency("org.mariadb.jdbc", "mariadb-java-client", version(3,3,0)))
             .include(dependency("org.hsqldb", "hsqldb", version(2,7,2)))
-            .include(dependency("org.apache.derby", "derby", version("10.16.1.1")))
-            .include(dependency("org.apache.derby", "derbytools", version("10.16.1.1")))
+            .include(dependency("org.apache.derby", "derby", version("10.17.1.0")))
+            .include(dependency("org.apache.derby", "derbytools", version("10.17.1.0")))
             .include(dependency("com.oracle.database.jdbc", "ojdbc11", version("23.3.0.23.09")));
 
         cleanOperation()

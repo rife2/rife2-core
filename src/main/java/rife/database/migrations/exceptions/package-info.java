9845f4601b44a18c320f327acb4fea085964f77b
Geert Bevin <gbevin@uwyn.com>
2026-07-25 14:24:50 -0400

    Added package javadocs for database migrations

---
+++ src/main/java/rife/database/migrations/exceptions/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2001-2026 Geert Bevin (gbevin[remove] at uwyn dot com)
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
+
+/**
+ * Provides exception classes for the database migrations.
+ * @since 1.10
+ */
+package rife.database.migrations.exceptions;
---
+++ src/main/java/rife/database/migrations/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2001-2026 Geert Bevin (gbevin[remove] at uwyn dot com)
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
+
+/**
+ * Provides classes for versioned database schema migrations that are
+ * declared with the object-oriented query builders and executed explicitly.
+ * @since 1.10
+ */
+package rife.database.migrations;
